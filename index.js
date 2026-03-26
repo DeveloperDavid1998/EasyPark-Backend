854ab0f8b67364bdbd75573ac0417bcaf1b3a4c4
David Asencios <asencios_52@hotmail.com>
2026-03-26 18:54:37 -0500

    Test ruta raiz

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-
+app.get('/', (req, res) => {
+  res.send('Backend funcionando');
+});
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
