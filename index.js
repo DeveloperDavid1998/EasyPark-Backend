10b1f14317f7112e5f4b4d025e774ef2d3dbcc55
David Asencios <asencios_52@hotmail.com>
2026-03-25 19:12:25 -0500

    Fix CORS para Vercel

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
